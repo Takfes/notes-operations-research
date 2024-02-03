467eb3eba9547609f331c11231f10bd1e41dc471
tfe <Takfes@users.noreply.github.com>
2024-02-03 11:56:53 +0200

    add genetic algorithm

--- src/C.udemy-ha/41.genetic-algorithms/genetic-algorithm.py
+++ src/C.udemy-ha/41.genetic-algorithms/genetic-algorithm.py
@@ -47,7 +47,7 @@ def binary_to_integer(binary_segment, verbose=False):
     Parameters:
         binary_segment (str): The binary segment to be converted.
         verbose (bool, optional): If True, the conversion process will be displayed step by step.
-                                  Defaults to False.
+        Defaults to False.
 
     Returns:
         int: The integer value of the binary segment.
@@ -117,7 +117,7 @@ def evaluate_chromosome(chromosome, length_per_variable, range_min, range_max):
     return Himmelblau(x_real, y_real), x_real, y_real
 
 
-def find_paretns_tournament(population, tournament_rounds):
+def tournament_operation(population, tournament_rounds, total_chromosome_length):
     """
     Selects parents using a tournament selection process.
 
@@ -144,19 +144,29 @@ def find_paretns_tournament(population, tournament_rounds):
     return parents
 
 
-def crossover_function(
-    chromosome1, chromosome2, length_per_variable, digits_to_change=2
+def crossover_operation(
+    chromosome1,
+    chromosome2,
+    length_per_variable,
+    digits_to_change=2,
+    crossover_probability=1.0,
 ):
     """
     Perform crossover operation on two chromosomes.
 
     Args:
-        chromosome1 (list): The first chromosome.
-        chromosome2 (list): The second chromosome.
+        chromosome1 (str): The first chromosome.
+        chromosome2 (str): The second chromosome.
+        length_per_variable (int): The length of each variable in the chromosomes.
+        digits_to_change (int, optional): The number of digits to change during crossover. Defaults to 2.
+        crossover_probability (float, optional): The probability of performing crossover. Defaults to 1.0.
 
     Returns:
-        tuple: A tuple containing two children chromosomes resulting from the crossover operation.
+        tuple: A tuple containing two child chromosomes resulting from crossover.
     """
+    if np.random.rand() >= crossover_probability:
+        return chromosome1, chromosome2
+
     idx1, idx2, idx3 = (
         length_per_variable - digits_to_change,
         length_per_variable,
@@ -176,6 +186,32 @@ def crossover_function(
     )
     assert len(child1) == len(chromosome1) == len(chromosome2) == len(child2)
     return child1, child2
+
+
+def mutation_operation(chromosome, mutation_probability=0.1):
+    """
+    Performs mutation operation on the given chromosome.
+
+    Args:
+        chromosome (str): The chromosome to be mutated.
+        mutation_probability (float, optional): The probability of mutation for each gene in the chromosome.
+            Defaults to 0.1.
+
+    Returns:
+        str: The mutated chromosome.
+    """
+    mutation_matrix = [
+        (np.random.rand() < mutation_probability) * 1 for _ in range(len(chromosome))
+    ]
+    return "".join([str(int(x) ^ y) for x, y in zip(chromosome, mutation_matrix)])
+
+
+def evaluate_operation(chomosomes):
+    scores = [
+        evaluate_chromosome(x, length_per_variable, range_min, range_max)[0]
+        for x in chomosomes
+    ]
+    print(f"best : {min(scores):.8f} out of {len(scores)} ")
 
 
 # Example usage for the Himmelblau function
@@ -217,9 +253,9 @@ Himmelblau(x_real, y_real)
 
 # 1. Tournament Selection
 TOURNAMENT_ROUNDS = 50
-POPULATION = 500
+POPULATION = 1000
 
-tournament_parents = find_paretns_tournament(
+tournament_parents = tournament_operation(
     population=POPULATION, tournament_rounds=TOURNAMENT_ROUNDS
 )
 eval_tournament_parents = [
@@ -235,9 +271,11 @@ print(
 crossover_childs = set()
 combs = list(combinations(tournament_parents, 2))
 for i, comb in enumerate(combs):
-    child1, child2 = crossover_function(comb[0], comb[1], length_per_variable)
+    child1, child2 = crossover_operation(comb[0], comb[1], length_per_variable)
     crossover_childs.add(child1)
     crossover_childs.add(child2)
+
+crossover_childs = list(crossover_childs)
 
 eval_crossover_childs = [
     evaluate_chromosome(x, length_per_variable, range_min, range_max)[0]
@@ -247,4 +285,80 @@ eval_crossover_childs = [
 print(
     f"CO best : {min(eval_crossover_childs):.8f} out of {len(eval_crossover_childs)} "
 )
+
+# 3. Mutation Operation
+mutated_childs = [mutation_operation(x, 0.1) for x in crossover_childs]
+eval_mutated_childs = [
+    evaluate_chromosome(x, length_per_variable, range_min, range_max)[0]
+    for x in mutated_childs
+]
+
+print(f"MO best : {min(eval_mutated_childs):.8f} out of {len(eval_mutated_childs)} ")
+
+# 4. Selection of the best chromosomes
+consolidated_chromosomes = list(
+    set(tournament_parents + crossover_childs + mutated_childs)
+)
+eval_consolidated_chromosomes = [
+    evaluate_chromosome(x, length_per_variable, range_min, range_max)[0]
+    for x in consolidated_chromosomes
+]
+evaluate_generation = zip(consolidated_chromosomes, eval_consolidated_chromosomes)
+evaluate_generation = sorted(evaluate_generation, key=lambda x: x[1])
+next_generation = [x[0] for x in evaluate_generation[:TOURNAMENT_ROUNDS]]
+
+
+# 5. Repeat
+def genetic_algorithm(
+    population_size,
+    tournament_rounds,
+    crossover_probability,
+    mutation_probability,
+    generations,
+    range_min,
+    range_max,
+    precision,
+):
+    length_per_variable = calculate_chromosome_length(range_min, range_max, precision)
+    total_chromosome_length = length_per_variable * 2
+    parents = tournament_operation(
+        population_size, tournament_rounds, total_chromosome_length
+    )
+    evaluate_operation(parents)
+    for _ in range(generations):
+        children = set()
+        combs = list(combinations(parents, 2))
+        for i, comb in enumerate(combs):
+            child1, child2 = crossover_operation(
+                comb[0], comb[1], length_per_variable, crossover_probability
+            )
+            children.add(child1)
+            children.add(child2)
+        children = list(children)
+        mutated_children = [
+            mutation_operation(x, mutation_probability) for x in children
+        ]
+        consolidated_chromosomes = list(set(parents + children + mutated_children))
+        eval_consolidated_chromosomes = [
+            evaluate_chromosome(x, length_per_variable, range_min, range_max)[0]
+            for x in consolidated_chromosomes
+        ]
+        evaluate_generation = zip(
+            consolidated_chromosomes, eval_consolidated_chromosomes
+        )
+        evaluate_generation = sorted(evaluate_generation, key=lambda x: x[1])
+        next_generation = [x[0] for x in evaluate_generation[:tournament_rounds]]
+        parents = next_generation
+    return next_generation
+
+
+ga = genetic_algorithm(
+    population_size=500,
+    tournament_rounds=50,
+    crossover_probability=1.0,
+    mutation_probability=0.2,
+    generations=10,
+    range_min=-5,
+    range_max=5,
+    precision=0.01,
 )
