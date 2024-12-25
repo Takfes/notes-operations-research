68e62f4ca543b81dd4bfcae201a817b52a98cb7c
tfe <Takfes@users.noreply.github.com>
2024-12-25 07:06:18 +0200

    enabled google-or-constraint-tsp solver

--- Coursera : Discrete Optimization/traveling_salesman/functions.py
+++ Coursera : Discrete Optimization/traveling_salesman/functions.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from ortools.constraint_solver import pywrapcp, routing_enums_pb2
+from tqdm import tqdm
 
 # Data Paths
 ROOT_DIR = Path(__file__).resolve().parent
@@ -88,14 +90,18 @@ def load_solution_from_disk(file_name, file_path=SOLUTION_DIR):
         return file.read()
 
 
-# TODO : Implement this function
-def generate_output():
-    pass
+def generate_output(sequence, distance_matrix):
+    obj = calculate_trip_sequence_from_distance_matrix(
+        distance_matrix=distance_matrix, sequence=sequence
+    )
+    answer = f"{obj} 0\n"
+    answer += " ".join(map(str, sequence[:-1]))
+    return answer
 
 
 """
 # ==============================================================
-# TSP functions
+# TSP custom functions
 # ==============================================================
 """
 
@@ -105,7 +111,7 @@ def euclidean_distance(point1x, point1y, point2x, point2y):
 
 
 def calculate_trip_sequence_from_distance_matrix(
-    distance_matrix, sequence, close_loop=False
+    distance_matrix, sequence, close_loop=True
 ):
     # Calculate total trip distance using the distance matrix
     total_distance = sum(
@@ -209,10 +215,9 @@ def calculate_distance_matrix(locations):
 
 
 def tsp_nearest_point_insertion(
-    distance_matrix, close_loop=False, start_point=0
+    distance_matrix, close_loop=True, start_point=0
 ):
     sequence = [start_point]
-    distances = []
     while len(sequence) < distance_matrix.shape[0]:
         # last element
         current_point = sequence[-1]
@@ -226,25 +231,21 @@ def tsp_nearest_point_insertion(
         )
         # add the next point to the sequence list
         sequence.append(next_point)
-        # add the distance between the current and next point to the distances list
-        distances.append(distance_matrix.iloc[current_point, next_point].item())
 
     if close_loop:
         sequence.append(sequence[0])
-        distances.append(
-            distance_matrix.iloc[sequence[-2], sequence[-1]].item()
-        )
 
-    assert len(sequence) == len(distances) + 1, (
-        "Sequence and distances mismatch"
-    )
-    return sequence, distances
+    return sequence
 
 
-# TODO : Greedy insertion heuristic
 def tsp_greedy_subtrip_insertion(
-    distance_matrix, start_point=0, subtrip_size=3, neighborhood_size=5
+    distance_matrix,
+    start_point=0,
+    subtrip_size=2,
+    neighborhood_size=None,
 ):
+    if neighborhood_size is None:
+        neighborhood_size = distance_matrix.shape[0] // 5
     # start by defining an initial subtrip
     sequence = [start_point]
     while len(sequence) < subtrip_size:
@@ -264,49 +265,212 @@ def tsp_greedy_subtrip_insertion(
     # close loop to finish the subtrip
     sequence.append(sequence[0])
 
-    # define neighborhood of the subtrip
-    # find distance of all points to the subtrip excluding the subtrip itself
-    # find the closest X points to the subtrip
-    neighborhood = (
-        distance_matrix.iloc[
-            list(set(sequence)), ~distance_matrix.index.isin(sequence)
-        ]
-        .melt(ignore_index=False)
-        .reset_index()
-        .rename(
-            columns={"index": "from", "variable": "to", "value": "distance"}
-        )
-        .nsmallest(neighborhood_size, "distance")
-    )
-
-    # track the candidates to insert into the subtrip
-    neighborhood_candidates = neighborhood.to.unique().tolist()
-
-    # candidate = neighborhood_candidates[0]
-
-    # find the best candidate to insert into the subtrip
-    new_subtrip_size = distance_matrix.sum().sum()
-    for candidate in neighborhood_candidates:
-        for posx in range(1, len(sequence)):
-            copy_sequence = sequence.copy()
-            print(f"Inserting {candidate=} at position {posx}")
-            copy_sequence.insert(posx, candidate)
-            print(copy_sequence)
-            candidate_size = calculate_trip_sequence_from_distance_matrix(
-                distance_matrix, copy_sequence, close_loop=True
+    # repeat the process until the sequence includes all points, that is the size of the distance matrix + 1, since the loop is closed
+    while len(sequence) <= distance_matrix.shape[0]:
+        # define neighborhood of the subtrip
+        # find distance of all points to the subtrip excluding the subtrip itself
+        # find the closest X points to the subtrip
+        neighborhood = (
+            distance_matrix.iloc[
+                list(set(sequence)), ~distance_matrix.index.isin(sequence)
+            ]
+            .melt(ignore_index=False)
+            .reset_index()
+            .rename(
+                columns={"index": "from", "variable": "to", "value": "distance"}
             )
-            print(candidate_size)
-            if candidate_size < new_subtrip_size:
-                new_subtrip_size = candidate_size
-                new_sequence = copy_sequence
-    # assert len(sequence) == len(distances) + 1, (
-    #     "Sequence and distances mismatch"
-    # )
-    # return sequence, distances
+            .nsmallest(neighborhood_size, "distance")
+        )
+
+        # track the candidates to insert into the subtrip
+        neighborhood_candidates = neighborhood.to.unique().tolist()
+
+        # find the best candidate to insert into the subtrip
+        new_subtrip_size = distance_matrix.sum().sum()
+        for candidate in neighborhood_candidates:
+            for posx in range(1, len(sequence)):
+                copy_sequence = sequence.copy()
+                # print(f"Inserting {candidate=} at position {posx}")
+                copy_sequence.insert(posx, candidate)
+                # print(copy_sequence)
+                candidate_size = calculate_trip_sequence_from_distance_matrix(
+                    distance_matrix, copy_sequence, close_loop=True
+                )
+                # print(candidate_size)
+                if candidate_size < new_subtrip_size:
+                    new_subtrip_size = candidate_size
+                    new_sequence = copy_sequence
+        sequence = new_sequence
+
+    assert len(sequence) == distance_matrix.shape[0] + 1
+    assert len(sequence[:-1]) == len(set(sequence))
+    assert sequence[0] == sequence[-1]
+
+    return sequence
 
 
-# TODO : Greedy swap heuristic
-# TODO : Greedy 2-opt heuristic
+def tsp_two_opt_improvement(distance_matrix, sequence):
+    # TODO : check whether the remaining process needs to be applied on the initial sequence or the best sequence
+    best_sequence = sequence.copy()
+    best_sequence_length = calculate_trip_sequence_from_distance_matrix(
+        distance_matrix, sequence
+    )
+    for j in range(1, len(sequence) - 1):
+        for i in range(1, j - 1):
+            start = sequence[:i]
+            middle = sequence[i:j]
+            end = sequence[j:]
+            new_sequence = start + list(reversed(middle)) + end
+            new_sequence_length = calculate_trip_sequence_from_distance_matrix(
+                distance_matrix, new_sequence
+            )
+            if new_sequence_length < best_sequence_length:
+                best_sequence_length = new_sequence_length
+                best_sequence = new_sequence
+
+    return best_sequence
+
+
+def tsp_swap_improvement(distance_matrix, sequence):
+    # TODO : check whether the remaining process needs to be applied on the initial sequence or the best sequence
+    best_sequence = sequence.copy()
+    best_sequence_length = calculate_trip_sequence_from_distance_matrix(
+        distance_matrix, sequence
+    )
+    for j in range(1, len(sequence) - 1):
+        for i in range(1, j):
+            new_sequence = sequence.copy()
+            # swap the cities
+            new_sequence[i], new_sequence[j] = (
+                new_sequence[j],
+                new_sequence[i],
+            )
+            new_sequence_length = calculate_trip_sequence_from_distance_matrix(
+                distance_matrix, new_sequence
+            )
+            if new_sequence_length < best_sequence_length:
+                best_sequence_length = new_sequence_length
+                best_sequence = new_sequence
+
+    return best_sequence
+
+
+def start_point_optimizer(distance_matrix, constructive_heuristic):
+    best_distance = distance_matrix.sum().sum()
+    best_seq = None
+    best_start_point = None
+    for x in tqdm(distance_matrix.index.tolist()):
+        temp_seq = constructive_heuristic(distance_matrix, start_point=x)
+        temp_seq_length = calculate_trip_sequence_from_distance_matrix(
+            distance_matrix, temp_seq
+        )
+        if temp_seq_length < best_distance:
+            best_distance = temp_seq_length
+            best_seq = temp_seq
+            best_start_point = x
+    print(
+        f"Start Point Optimization : {best_distance=:.2f}, {best_start_point=:.2f} | Step function: {constructive_heuristic.__name__}"
+    )
+    return best_seq, best_distance, best_start_point
+
+
+def hill_climb_corrective(
+    distance_matrix, sequence, step_function, max_iter=100
+):
+    best_sequence = sequence.copy()
+    best_sequence_length = calculate_trip_sequence_from_distance_matrix(
+        distance_matrix, best_sequence
+    )
+    counter = 0
+
+    while True:
+        new_sequence = step_function(distance_matrix, best_sequence)
+        new_sequence_length = calculate_trip_sequence_from_distance_matrix(
+            distance_matrix, new_sequence
+        )
+        counter += 1
+        if new_sequence_length < best_sequence_length and counter <= max_iter:
+            best_sequence = new_sequence
+            best_sequence_length = new_sequence_length
+        else:
+            print(
+                f"Hill Climb : Best sequence length: {best_sequence_length:.2f} | Iterations: {counter} | Step function: {step_function.__name__}"
+            )
+            return best_sequence
+
+
 # TODO : Simulated Annealing
 # TODO : Tabu Search
-# TODO : or-tools TSP solver
+
+"""
+# ==============================================================
+# TSP or tools constraint solver
+# ==============================================================
+"""
+
+
+def tsp_or_constraint_solver(
+    distance_matrix, search_strategy="guided", logging=False, time_limit=None
+):
+    """_summary_
+
+    Args:
+        distance_matrix (pd.DataFrame): _description_
+        search_strategy (str, optional): guided or first. Defaults to 'guided'.
+        logging (bool, optional): _description_. Defaults to False.
+        time_limit (_type_, optional): _description_. Defaults to None.
+    """
+
+    def get_routes(solution, routing, manager):
+        """Get vehicle routes from a solution and store them in an array."""
+        # Get vehicle routes and store them in a two dimensional array whose
+        # i,j entry is the jth location visited by vehicle i along its route.
+        routes = []
+        for route_nbr in range(routing.vehicles()):
+            index = routing.Start(route_nbr)
+            route = [manager.IndexToNode(index)]
+            while not routing.IsEnd(index):
+                index = solution.Value(routing.NextVar(index))
+                route.append(manager.IndexToNode(index))
+            routes.append(route)
+        return routes
+
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return matrix[from_node][to_node]
+
+    # Instantiate the data problem.
+    matrix = [list(map(int, row)) for row in distance_matrix.values]
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(matrix), 1, 0)
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+    # Register a transit callback.
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    # Setting the search strategy
+    if search_strategy == "first":
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+        )
+    elif search_strategy == "guided":
+        search_parameters.local_search_metaheuristic = (
+            routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
+        )
+    # Setting the time limit
+    if time_limit is not None:
+        search_parameters.time_limit.seconds = 30
+    # Setting the logging
+    search_parameters.log_search = logging
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+    routes = get_routes(solution, routing, manager)
+    sequence = routes[0]
+
+    return sequence
--- Coursera : Discrete Optimization/traveling_salesman/main.py
+++ Coursera : Discrete Optimization/traveling_salesman/main.py
@@ -1,15 +1,23 @@
-import time
 from pathlib import Path
 
 from functions import (
     DATA_DIR,
     calculate_distance_matrix,
+    calculate_trip_sequence_from_distance_matrix,
     calculate_trip_sequence_from_locations,
+    generate_output,
+    hill_climb_corrective,
     list_files_in_dir,
     parse_input_data,
     plot_tsp_trip,
+    start_point_optimizer,
+    tsp_greedy_subtrip_insertion,
     tsp_nearest_point_insertion,
+    tsp_or_constraint_solver,
+    tsp_swap_improvement,
+    tsp_two_opt_improvement,
 )
+from matplotlib.pyplot import plot
 
 """
 tsp_51_1     Traveling Salesman Problem 1
@@ -21,21 +29,33 @@ tsp_33810_1  Traveling Salesman Problem 6
 """
 
 # * DEFINE CONSTANTS
-file_name = "tsp_51_1"
+START_POINT_OPTIMIZATION = False
+file_name = "tsp_200_2"
 
 # * LOAD DATA
 list_files_in_dir(full_path=False)
 input_data = Path(DATA_DIR) / file_name
 data = parse_input_data(input_data, input_type="file")
-
-n_locations = data["n_locations"]
 locations = data["locations"]
-
 dm = calculate_distance_matrix(locations)
-dm.head()
 
-sequence, distances = tsp_nearest_point_insertion(dm, close_loop=False)
+# * TSP OR TOOLS SOLVER
+seqor = tsp_or_constraint_solver(dm, "guided", time_limit=30, logging=True)
+calculate_trip_sequence_from_distance_matrix(dm, seqor)
+plot_tsp_trip(locations, sequence=seqor, title=file_name)
+output_data = generate_output(seqor, dm)
+print(output_data)
 
-calculate_trip_sequence_from_locations(locations, sequence, close_loop=True)
+# * TSP CUSTOM HEURISTICS
+if START_POINT_OPTIMIZATION:
+    seq_spo, _, _ = start_point_optimizer(dm, tsp_greedy_subtrip_insertion)
+else:
+    seq_spo = tsp_greedy_subtrip_insertion(dm)
 
-plot_tsp_trip(locations, sequence=sequence, close_loop=False, title=file_name)
+seq_spo_ht = hill_climb_corrective(dm, seq_spo, tsp_two_opt_improvement)
+seq_spo_ht_hs = hill_climb_corrective(dm, seq_spo_ht, tsp_swap_improvement)
+
+calculate_trip_sequence_from_distance_matrix(dm, seq_spo_ht_hs)
+plot_tsp_trip(locations, sequence=seq_spo_ht_hs, title=file_name)
+output_data = generate_output(seq_spo_ht_hs, dm)
+print(output_data)
